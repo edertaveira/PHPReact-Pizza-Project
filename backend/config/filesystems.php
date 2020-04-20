e38948245b43ca92c0bcb4f73e61c7022721bc46
Eder <eder.t@turing.com>
2020-04-19 22:45:30 -0300

    Updating filesystems

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -50,7 +50,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path(),
+            //'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
